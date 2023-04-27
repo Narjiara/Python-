e14eb7f8942d121929e808b6915209e7eb111b51
Narjiara <114522575+Narjiara@users.noreply.github.com>
2023-04-27 17:36:37 -0300

    Add files via upload

---
+++ revisao1.py
@@ -0,0 +1,20 @@
+continuar = 's'
+lista = []
+while continuar == 's':
+      nota1 = int(input("digite 1 nota:"))
+      nota2 = int(input("digite 2 nota:"))
+      media = nota1 + nota2 / 2
+      lista.append(media)
+      if media >= 7:
+        print("aluno aprovado")
+      elif media <= 4:
+        print("aluno recuperacao")
+
+      else:
+         print("aluno reprovado")
+      continuar = input("quer continuar:")
+
+      print(lista)
+
+
+
