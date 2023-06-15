572efdf37a43fe01911f061a1519010df9ce500c
narjiara <narjiarabatista447@gmail.com>
2023-06-15 16:30:05 -0300

    banco de dados

---
+++ newProject/ativid1.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+banco = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "cdd2023",
+    database = "escola_turmac"
+)
+print(banco)
+
+meucursor = banco.cursor()
+pesquisa = 'select * from alunos;'
+meucursor.execute(pesquisa)
+resultado = meucursor.fetchall()
+for x in resultado:
+    print(x)
+meucursor.close()
+banco.close()
