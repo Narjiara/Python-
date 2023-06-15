429cd32aa47fecfcc0d42d6a64a8191ad6ede714
narjiara <narjiarabatista447@gmail.com>
2023-06-15 16:49:31 -0300

    banco de dados

--- newProject/ativid1.py
+++ newProject/ativid1.py
@@ -9,10 +9,23 @@ banco = mysql.connector.connect(
 print(banco)
 
 meucursor = banco.cursor()
-pesquisa = 'select * from funcionarios;'
+pesquisa = 'select * from alunos;'
 meucursor.execute(pesquisa)
 resultado = meucursor.fetchall()
 for x in resultado:
     print(x)
+
+
+nome1 = "rosa"
+telefone1 = "67895432091"
+turma1 = "C"
+media1 = "7"
+meucursor = banco.cursor()
+sql = "insert into alunos (nome,telefone,turma,media) values (%s, %s,%s,%s)"
+data = (nome1,telefone1,turma1,media1)
+meucursor.execute(sql,data)
+banco.commit()
+userid = meucursor.lastrowid
+print(userid)
 meucursor.close()
 banco.close()
